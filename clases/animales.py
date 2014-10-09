5e2a0ff4c8400aed32a67f974f021074aac80392
Israel <asralf@gmail.com>
2014-10-09 11:34:50 -0500

    modificacion animales.py

--- clases/animales.py
+++ clases/animales.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class Animal():
 
   def __init__(self,**args):
@@ -32,34 +32,3 @@ class Perro(Animal):
 
   def getNombrePropio(self):
     return self.NombrePropio
-
-
-=======
-
-
-class Animal():
-
-    def __init__(self,**args):
-        self.nombreCientifico = args["nombreCientifico"] if "nombreCientifico" in args else ""
-
-    def setNombreCientifico(self,nombreCientifico):
-        self.nombreCientifico = nombreCientifico
-
-    def setNombreComun(self, nombreComun):
-        self.nombreComun = nombreComun
-
-    def getNombreCientifico(self):
-        return self.nombreCientifico
-
-    def queSoy(self):
-        return "Soy un animal"
-
-class Perro(Animal):
-  pass
->>>>>>> upstream/master
-
-class Gato(Animal):
-  pass
-
-class Puerco(Animal):
-  pass
